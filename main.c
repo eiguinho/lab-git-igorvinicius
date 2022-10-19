cb8ec844cb74dec0c097f4a4e89a76cc53118790
eiguinho <igorvinijesus@gmail.com>
2022-10-19 17:41:12 -0300

    Incluindo a funcao somatoriaRecursiva

--- main.c
+++ main.c
@@ -10,6 +10,18 @@ void somatoria(int x, int y){
     printf("\nA somatoria é %ld\n", soma);
 }
 
+long int somatoriaRecursiva(int x, int y) {
+    if(x<=y){
+        long int soma = x;
+            long int resultado = somatoriaRecursiva(x+1, y);
+            printf("%ld - ", resultado);
+        return soma+resultado;
+    }
+    else{
+        return 0;
+    }
+}
+
 int main(void) {
     printf("\n\n :::::: INICIO DO PROGRAMA :::::: \n\n");
     printf("\n\nDigite o número x: ");
@@ -17,6 +29,6 @@ int main(void) {
     printf("\n\nDigite o número y: ");
     scanf("%d", &y);
 
-    soma(x,y);
+    printf("\n A somatória é: %ld", somatoriaRecursiva(x, y));
     return 0;
 }
